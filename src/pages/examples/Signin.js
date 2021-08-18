b865bcfecd78324842582477b9c30d790e56b9bd
Wiifek <chabir.wifek@gmail.com>
2021-08-18 10:27:21 +0100

    add validation

--- src/pages/examples/Signin.js
+++ src/pages/examples/Signin.js
@@ -10,31 +10,63 @@ import authService from "../../services/auth.service";
 import { Routes } from "../../routes";
 import BgImage from "../../assets/img/illustrations/signin.svg";
 
+import { ToastContainer } from "react-toastr";
+import toasterService from "../../services/toaster.service";
+import "toastr/build/toastr.css";
+import "animate.css/animate.css";
+
 export default () => {
   let history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailRequired, setEmailRequired] = useState(null);
+  const [passwordRequired, setPasswordRequired] = useState(null);
 
-  const submitHandler=(e)=>{
+  let container;
+
+  const isValidForm =()=>{
+    if (!email){
+      setEmailRequired('Email address is required.')
+    }
+    else{
+      setEmailRequired(null)
+    }
+    if (!password){
+      setPasswordRequired('Password is required.')
+    }
+    else{
+      setPasswordRequired(null)
+    }
+    if(email && password){
+      return true
+    }
+    else return false
+  }
+
+  const submitHandler = (e) => {
     e.preventDefault();
-    authService.login({email, password})
-    .then(response => {
-      localStorage.setItem('token', response.data.token);
-      history.push("/dashboard/overview")
-    }).catch(err => {
-      console.log(err)
-    });
-
+    if (isValidForm()){
+    authService.login({ email, password })
+      .then(response => {
+        toasterService.showSuccessMessage(container, "Welcome to your access panel", `Sign in successful`)
+        localStorage.setItem('token', response.data.token);
+        // history.push("/dashboard/overview")
+      }).catch(err => {
+        toasterService.showErrorMessage(container, "You have entered an invalid username or password", `Sign in failed`)
+        console.log(err)
+      });
+    }
   }
   return (
     <main>
       <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
         <Container>
-          <p className="text-center">
-            <Card.Link as={Link} to={Routes.DashboardOverview.path} className="text-gray-700">
-              <FontAwesomeIcon icon={faAngleLeft} className="me-2" /> Back to homepage
-            </Card.Link>
-          </p>
+        <ToastContainer
+          className="toast-top-right"
+          ref={ref => {
+            container = ref;
+          }}
+        />
           <Row className="justify-content-center form-bg-image" style={{ backgroundImage: `url(${BgImage})` }}>
             <Col xs={12} className="d-flex align-items-center justify-content-center">
               <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
@@ -48,7 +80,8 @@ export default () => {
                       <InputGroup.Text>
                         <FontAwesomeIcon icon={faEnvelope} />
                       </InputGroup.Text>
-                      <Form.Control autoFocus required value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="example@company.com" />
+                      <Form.Control autoFocus value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="example@company.com" />
+                      <div className="text-start w-100 d-block invalid-feedback">{emailRequired}</div>
                     </InputGroup>
                   </Form.Group>
                   <Form.Group>
@@ -58,7 +91,8 @@ export default () => {
                         <InputGroup.Text>
                           <FontAwesomeIcon icon={faUnlockAlt} />
                         </InputGroup.Text>
-                        <Form.Control required value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+                        <Form.Control value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+                        <div className="text-start w-100 d-block invalid-feedback">{passwordRequired}</div>
                       </InputGroup>
                     </Form.Group>
                     <div className="d-flex justify-content-between align-items-center mb-4">
@@ -66,7 +100,7 @@ export default () => {
                         <FormCheck.Input id="defaultCheck5" className="me-2" />
                         <FormCheck.Label htmlFor="defaultCheck5" className="mb-0">Remember me</FormCheck.Label>
                       </Form.Check>
-                      <Card.Link className="small text-end">Lost password?</Card.Link>
+                      <Card.Link as={Link} to={Routes.ForgotPassword.path} className="small text-end">Lost password?</Card.Link>
                     </div>
                   </Form.Group>
                   <Button variant="primary" type="submit" className="w-100">
